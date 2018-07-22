cafe9192fcf45ea99608995274ecaa0f34384552
Alex Litvin <alexlitvin6969@gmail.com>
2018-07-22 17:58:46 +0300

    add RegistrationServlet change servlet dependency to 3.1.0 add login.html and register.html

--- src/main/java/ua/training/model/entity/Contact.java
+++ src/main/java/ua/training/model/entity/Contact.java
@@ -2,22 +2,21 @@ package ua.training.model.entity;
 
 import lombok.Data;
 
-import java.time.LocalDateTime;
-
 @Data
 class Contact {
     private Integer id;
     private String firstName;
     private String lastName;
-    private String middleName;
-    private String shortName;
+//    private String middleName;
+//    private String shortName;
     private String login;
-    private String comment;
-    private String homePhone;
-    private String mobile;
-    private String email;
-    private String skype;
-    private Address address;
-    private LocalDateTime creationDate;
-    private LocalDateTime modificationDate;
+    private String password;
+//    private String comment;
+//    private String homePhone;
+//    private String mobile;
+//    private String email;
+//    private String skype;
+//    private Address address;
+//    private LocalDateTime creationDate;
+//    private LocalDateTime modificationDate;
 }
--- src/main/java/ua/training/servlet/RegistrationServlet.java
+++ src/main/java/ua/training/servlet/RegistrationServlet.java
@@ -1,11 +1,11 @@
-package ua.training;
+package ua.training.servlet;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class RegistrationForm extends HttpServlet {
+public class RegistrationServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.getWriter().print("Hello from servlet");
     }
